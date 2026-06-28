6fad78ba9edae94b580916af79a26caf04dda2fd
s-sato <sayaka000521@gmail.com>
2026-06-28 22:03:34 +0900

    入力テキスト画面表示

--- frontend/todo-app/src/App.tsx
+++ frontend/todo-app/src/App.tsx
@@ -1,6 +1,28 @@
+import { useState } from "react";
+import type { ChangeEvent } from "react";
+
 function App() {
+  /* 変数nameにsetNameを利用して値を入れる */
+  const [name, setName] = useState("");
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
+
   return (
-    <h1>Hello World</h1>
+    <>
+      <h1>名前を入力してください。</h1>
+
+      <input
+        type="text"
+        className="name"
+        name="name"
+        value={name}
+        onChange={handleChange}
+      />
+
+      <p>入力した名前：{name}</p>
+    </>
   );
 }
 
